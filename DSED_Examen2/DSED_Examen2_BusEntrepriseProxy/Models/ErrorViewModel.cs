2f4e31b0d2e910724cd551ba0332f4deb1d68a87
ZacharyGagnon <zac08gag@gmail.com>
2024-02-05 15:29:48 -0500

    commit

--- DSED_Examen2/DSED_Examen2_BusEntrepriseProxy/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DSED_Examen2_BusEntrepriseProxy.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
